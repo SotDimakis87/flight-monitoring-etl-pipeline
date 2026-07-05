ef1ad8f24b4d878214158bd55541ba234e8b31dc
Sotiris Dimakis <sotdimakis87@gmail.com>
2026-07-05 21:17:20 +0300

    init.sql updated type

--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -5,29 +5,29 @@ CREATE TABLE IF NOT EXISTS flights (
     flight_status TEXT,
 
     airline_name TEXT,
-    airline_iata TEXT,
-    airline_icao TEXT,
+    airline_iata CHAR(2),
+    airline_icao CHAR(3),
 
     flight_number TEXT,
     flight_iata TEXT,
     flight_icao TEXT,
 
     departure_airport TEXT,
-    departure_iata TEXT,
-    departure_icao TEXT,
+    departure_iata CHAR(3),
+    departure_icao CHAR(4),
     departure_terminal TEXT,
     departure_gate TEXT,
     departure_delay INTEGER,
-    scheduled_departure TIMESTAMP,
-    estimated_departure TIMESTAMP,
-    actual_departure TIMESTAMP,
+    scheduled_departure TIMESTAMPTZ,
+    estimated_departure TIMESTAMPTZ,
+    actual_departure TIMESTAMPTZ,
 
     arrival_airport TEXT,
-    arrival_iata TEXT,
-    arrival_icao TEXT,
-    scheduled_arrival TIMESTAMP,
-    estimated_arrival TIMESTAMP,
-    actual_arrival TIMESTAMP,
+    arrival_iata CHAR(3),
+    arrival_icao CHAR(4),
+    scheduled_arrival TIMESTAMPTZ,
+    estimated_arrival TIMESTAMPTZ,
+    actual_arrival TIMESTAMPTZ,
 
-    etl_load_timestamp TIMESTAMP
+    etl_load_timestamp TIMESTAMPTZ
 );
